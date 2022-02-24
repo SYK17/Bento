86c5e8bdb782cadd36621fb9046973f02b9ad544
SYK17 <23228396+SYK17@users.noreply.github.com>
2022-02-24 10:32:58 -0800

    Update config.js

--- config.js
+++ config.js
@@ -39,12 +39,12 @@ const CONFIG = {
   autoChangeTheme: false,
 
   // Autochabge by OS
-  changeThemeByOS: true,
+  changeThemeByOS: false,
 
   // Autochange by hour options (24hrs format, string must be in: hh:mm)
-  changeThemeByHour: false,
-  hourDarkThemeActive: '18:30',
-  hourDarkThemeInactive: '07:00',
+  changeThemeByHour: true,
+  hourDarkThemeActive: '18:00',
+  hourDarkThemeInactive: '06:00',
 
   // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
   // ├┴┐│ │ │  │ │ ││││└─┐
